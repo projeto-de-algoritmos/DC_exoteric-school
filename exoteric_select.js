d66c5f86a1360ceaea68849fad6b1c4314783311
Thiago Gomes <thiagosiqgomes@gmail.com>
2021-09-27 14:26:46 -0300

    implementa a funcao oraculo

--- exoteric_select.js
+++ exoteric_select.js
@@ -17,8 +17,42 @@ function exoteric_select (a, k) {
         return exoteric_select(right, k - left.length - 1)
 }
 
+
 function oracle (a) {
-    const randomIndex = Math.floor(Math.random() * (a.length - 0)) + 0;
-    return a[randomIndex]
+    if (a.length < 25) {
+        const randomIndex = Math.floor(Math.random() * (a.length - 0)) + 0;
+        return a[randomIndex]
+    } else {
+        const groups = []
+        for (let i = 0; i < 5; i++) {
+            groups[i] = []
+        }
+        for (let i = 0; i < 25; i+= 5) {
+            groups[1].push(a[i])
+            groups[2].push(a[i+1])
+            groups[3].push(a[i+2])
+            groups[4].push(a[i+3])
+            groups[5].push(a[i+4])
+        }
+
+        for (let i = 0; i < 5; i++) {
+            groups[i].sort(function(a, b) {
+                return a - b;
+            });
+        }
+
+        const medians = []
+
+        for (let i = 0; i < 5; i++) {
+            medians.push(groups[i][2])
+        }
+
+        medians.sort(function(a, b) {
+            return a - b;
+        });
+
+        console.log('mediana das medianas = ', median[2])
+        return median[2]
+    }
 }
 
