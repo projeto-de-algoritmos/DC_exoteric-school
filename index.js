081daeee7eb13dd64d8819c2f33375008b88b897
Thiago Gomes <thiagosiqgomes@gmail.com>
2021-09-27 23:09:08 -0300

    Adiciona alunos via javascrit

---
+++ index.js
@@ -0,0 +1,37 @@
+(function (DOM, doc, win) {
+    const alunos = [
+        {
+            nome: 'Thiago Siqueira Gomes',
+            nota: 10
+        },
+        {
+            nome: 'Christian Fleury Alencar Siqueira',
+            nota: 3
+        },
+        {
+            nome: 'Jairo Coelho da Mata',
+            nota: 1
+        },
+    ]
+
+    const $listaAlunos = doc.getElementById('lista-alunos')
+
+    alunos.forEach(element => {
+        const $li = doc.createElement('li')
+        const $name = doc.createElement('span')
+        const $result = doc.createElement('span')
+
+        $name.textContent = element.nome
+        $result.textContent = element.nota
+
+
+        $li.appendChild($name)
+        $li.appendChild($result)
+        
+        $listaAlunos.appendChild($li)
+    })
+
+
+    
+  
+})(window.DOM, document, window)
