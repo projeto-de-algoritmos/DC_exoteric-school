a5026cb4d32794eed029e3393da581340c4c4851
Christian Fleury <chfleurysiq@gmail.com>
2021-09-27 23:33:42 -0300

    adding notaAlunos

--- index.js
+++ index.js
@@ -1,117 +1,91 @@
 (function (DOM, doc, win) {
-    const alunos = [
-        {
-            nome: 'Thiago Siqueira Gomes',
-            nota: 10
-        },
-        {
-            nome: 'Christian Fleury Alencar Siqueira',
-            nota: 3
-        },
-        {
-            nome: 'Jairo Coelho da Mata',
-            nota: 1
-        },
-    ]
+  const alunos = [
+    {
+      nome: 'Thiago Siqueira Gomes',
+      nota: 10,
+    },
+    {
+      nome: 'Christian Fleury Alencar Siqueira',
+      nota: 3,
+    },
+    {
+      nome: 'Jairo Coelho da Mata',
+      nota: 1,
+    },
+  ];
 
-    const $listaAlunos = doc.getElementById('lista-alunos')
+  const notaAlunos = alunos.map((e) => {
+    return e.nota;
+  });
 
-    alunos.forEach(element => {
-        const $li = doc.createElement('li')
-        const $name = doc.createElement('span')
-        const $result = doc.createElement('span')
+  const $listaAlunos = doc.getElementById('lista-alunos');
 
-        $name.textContent = element.nome
-        $result.textContent = element.nota
+  alunos.forEach((element) => {
+    const $li = doc.createElement('li');
+    const $name = doc.createElement('span');
+    const $result = doc.createElement('span');
 
+    $name.textContent = element.nome;
+    $result.textContent = element.nota;
 
-        $li.appendChild($name)
-        $li.appendChild($result)
-        
-        $listaAlunos.appendChild($li)
-    })
+    $li.appendChild($name);
+    $li.appendChild($result);
 
-    
-    const $buttonBuscaAluno = doc.getElementById('button-busca-aluno')
-    $buttonBuscaAluno.addEventListener('click', function () {
-        
-    }, false)
+    $listaAlunos.appendChild($li);
+  });
 
+  const $buttonBuscaAluno = doc.getElementById('button-busca-aluno');
+  $buttonBuscaAluno.addEventListener('click', function () {}, false);
 
+  function exoteric_select(a, k) {
+    const median = oracle(a);
+    const right = [];
+    const left = [];
+    a.forEach((element) => {
+      if (element < median) left.push(element);
+      else if (element > median) right.push(element);
+    });
+    console.log(11);
+    if (left.length === k - 1) return median;
+    if (left.length > k - 1) return exoteric_select(left, k);
+    else return exoteric_select(right, k - left.length - 1);
+  }
 
+  function oracle(a) {
+    if (a.length < 25) {
+      const randomIndex = Math.floor(Math.random() * (a.length - 0)) + 0;
+      return a[randomIndex];
+    } else {
+      const groups = [];
+      for (let i = 0; i < 5; i++) {
+        groups[i] = [];
+      }
+      for (let i = 0; i < 25; i += 5) {
+        groups[1].push(a[i]);
+        groups[2].push(a[i + 1]);
+        groups[3].push(a[i + 2]);
+        groups[4].push(a[i + 3]);
+        groups[5].push(a[i + 4]);
+      }
 
+      for (let i = 0; i < 5; i++) {
+        groups[i].sort(function (a, b) {
+          return a - b;
+        });
+      }
 
+      const medians = [];
 
+      for (let i = 0; i < 5; i++) {
+        medians.push(groups[i][2]);
+      }
 
+      medians.sort(function (a, b) {
+        return a - b;
+      });
 
-
-
-    function exoteric_select (a, k) {
-        const median = oracle(a)
-        const right = []
-        const left = []
-        a.forEach(element => {
-            if (element < median)
-                left.push(element)
-            else if (element > median)
-                right.push(element)
-        })
-        console.log(11)
-        if (left.length === k - 1)
-            return median
-        if (left.length > k - 1)
-            return exoteric_select(left, k)
-        else
-            return exoteric_select(right, k - left.length - 1)
+      console.log('mediana das medianas = ', median[2]);
+      return median[2];
     }
-    
-    
-    function oracle (a) {
-        if (a.length < 25) {
-            const randomIndex = Math.floor(Math.random() * (a.length - 0)) + 0;
-            return a[randomIndex]
-        } else {
-            const groups = []
-            for (let i = 0; i < 5; i++) {
-                groups[i] = []
-            }
-            for (let i = 0; i < 25; i+= 5) {
-                groups[1].push(a[i])
-                groups[2].push(a[i+1])
-                groups[3].push(a[i+2])
-                groups[4].push(a[i+3])
-                groups[5].push(a[i+4])
-            }
-    
-            for (let i = 0; i < 5; i++) {
-                groups[i].sort(function(a, b) {
-                    return a - b;
-                });
-            }
-    
-            const medians = []
-    
-            for (let i = 0; i < 5; i++) {
-                medians.push(groups[i][2])
-            }
-    
-            medians.sort(function(a, b) {
-                return a - b;
-            });
-    
-            console.log('mediana das medianas = ', median[2])
-            return median[2]
-        }
-    }
-    
-    
-
-
-    
-
-
-
-
-    
-  
-})(window.DOM, document, window)
+  }
+})(window.DOM, document, window);
